431d3140928e6368626d82e57d3ba9d2d37b98fb
Emerson Huaman <emerson.huaman1@unmsm.edu.pe>
2023-05-11 18:18:41 -0500

    Atributos

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author Emerson
- */
+import java.util.Date;
+
 public class Cliente {
-    
+    private String numDoc;
+    private String nombre;
+    private Date nacimiento;
 }
--- src/modelo/Tarjeta.java
+++ src/modelo/Tarjeta.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author Emerson
- */
+import java.util.Date;
+
 public class Tarjeta {
-    
+    private String nombre;
+    private String numero;
+    private Date vencimiento;
+    private String cvv;
 }
